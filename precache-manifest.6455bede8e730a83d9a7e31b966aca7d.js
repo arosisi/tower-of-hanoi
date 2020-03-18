25832579a5905818a2c24176373fec24ed94d354
Tom Nguyen <tom.trinh.nguyen@gmail.com>
2020-03-18 12:50:23 -0400

    Updates

---
+++ precache-manifest.6455bede8e730a83d9a7e31b966aca7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130a3f27e2d5149ab6b0a045f4f15f15",
+    "url": "/tower-of-hanoi/index.html"
+  },
+  {
+    "revision": "28641fdaff347f9fe0d5",
+    "url": "/tower-of-hanoi/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "19106cc1604255dac875",
+    "url": "/tower-of-hanoi/static/js/2.4babfebe.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/tower-of-hanoi/static/js/2.4babfebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28641fdaff347f9fe0d5",
+    "url": "/tower-of-hanoi/static/js/main.9e56d54a.chunk.js"
+  },
+  {
+    "revision": "161932064034a209f319",
+    "url": "/tower-of-hanoi/static/js/runtime-main.eff730e6.js"
+  }
+]);
